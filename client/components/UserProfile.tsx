2e8bffc698dfe0f51d121fef56959e7a3b62242a
Katie Riches <katiegee2011@gmail.com>
2023-11-30 10:38:34 +1300

    Profile data displaying

--- client/components/UserProfile.tsx
+++ client/components/UserProfile.tsx
@@ -12,6 +12,7 @@ export default function UserProfile() {
     ;(async () => {
       const accessToken = await getAccessToken(clientId, code)
       const profile = await getProfile(accessToken)
+      console.log(profile)
       populateUI(profile)
     })()
   }
@@ -79,12 +80,37 @@ export default function UserProfile() {
     return access_token
   }
 
-  async function getProfile(token: string) {
+  async function getProfile(token: string): Promise<any> {
     //TODO: call web api
+
+    const result = await fetch('https://api.spotify.com/v1/me', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return await result.json()
   }
 
   function populateUI(profile: any) {
     //TODO: populate UI
+
+    document.getElementById('displayName')!.innerText = profile.display_name
+    if (profile.images[0]) {
+      const profileImage = new Image(200, 200)
+      profileImage.src = profile.images[0].url
+      document.getElementById('avatar')!.appendChild(profileImage)
+    }
+    document.getElementById('id')!.innerText = profile.id
+    document.getElementById('email')!.innerText = profile.email
+    document.getElementById('uri')!.innerText = profile.uri
+    document
+      .getElementById('uri')!
+      .setAttribute('href', profile.external_urls.spotify)
+    document.getElementById('url')!.innerText = profile.href
+    document.getElementById('url')!.setAttribute('href', profile.href)
+    document.getElementById('imgUrl')!.innerText =
+      profile.images[0]?.url ?? '(no profile image)'
   }
 
   return <Profile />
