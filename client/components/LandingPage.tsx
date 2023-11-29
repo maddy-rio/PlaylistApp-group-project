6f80815e7cf71011a396641d146106e13d2c084c
Karl Young <Karlyoung112@gmail.com>
2023-11-30 12:05:52 +1300

    Adding base components

---
+++ client/components/LandingPage.tsx
@@ -0,0 +1,20 @@
+
+const LandingPage = () => {
+
+
+
+  return (
+    <>
+    <div>
+      <h1>Daily Themes,
+        <br></br>
+        Collective Tunes
+      </h1>
+      <p>Collaborate with your friends, family, or workmates to build a memorable playlist, one song per day!</p>
+      <button>Sign in with Spotify</button>
+    </div>
+    </>
+  )
+}
+
+export default LandingPage()
