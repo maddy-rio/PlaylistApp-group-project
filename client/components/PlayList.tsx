6c44f0197bdabb0defe97421b197b2eacd8218f0
Karl Young <Karlyoung112@gmail.com>
2023-11-30 15:31:02 +1300

    Working on playlist comp

---
+++ client/components/PlayList.tsx
@@ -0,0 +1,33 @@
+import {User, Playlists} from '../../models/playlist'
+
+const PlaylistPage = ({Playlists, User})  => {
+  const renderPlaylists = () => {
+    if (Playlists.length > 0) {
+      return (
+        <div>
+          <h2>Hi {User.id}! Here&apos;s all the playlists your&apos;re collaborating on: </h2>
+          {Playlists.map((Playlist) => (
+            <div key={Playlist.id}>
+              <h3>{Playlist.playlistName}</h3>
+            </div>
+          ))}
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h2>Hi {User.id}!
+          <br></br>
+          Get started by adding a playlist.
+          </h2>
+        </div>
+      )
+    }
+
+  }
+  return (<>
+  <div>{renderPlaylists()}</div>
+  </>)
+} 
+
+export default PlaylistPage
