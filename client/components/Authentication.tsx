0a74e2d2775e7d3c9386d6f857434ce498c6e843
Murray Sinclair <msin981@gmail.com>
2023-11-30 22:45:44 +1300

    feat: Authentication Login page | firstpass

---
+++ client/components/Authentication.tsx
@@ -0,0 +1,38 @@
+import { useAuth0 } from '@auth0/auth0-react'
+
+function Authentication() {
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
+    useAuth0()
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  if (error) {
+    return <div>Oops... {error.message}</div>
+  }
+
+  if (isAuthenticated) {
+    return (
+      <div>
+        Hello {user.name}{' '}
+        <button
+          onClick={() =>
+            logout({ logoutParams: { returnTo: window.location.origin } })
+          }
+        >
+          Log out
+        </button>
+      </div>
+    )
+  } else {
+    return (
+      <>
+        <button onClick={() => loginWithRedirect()}>Log in</button>
+        <h1>Authentication page</h1>
+        <h3>User not logged in yet</h3>
+      </>
+    )
+  }
+}
+
+export default Authentication
