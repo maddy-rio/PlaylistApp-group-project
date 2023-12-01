e6c871b622e1f7713d507cfab9d428780f665d88
Murray Sinclair <msin981@gmail.com>
2023-12-01 21:35:28 +1300

    feat: Routes added

--- client/routes/test.tsx
+++ client/routes/test.tsx
@@ -1,10 +1,13 @@
 import { Route, createRoutesFromElements } from 'react-router-dom'
 import Authentication from '../components/Authentication'
 import Placeholder from '../components/Placeholder'
+import Layout from '../components/Layout'
+import Home from '../components/Home'
 
 export const routes = createRoutesFromElements(
   <>
-    <Route path="/">
+    <Route path="/" element={<Layout />}>
+      <Route path="home" element={<Home />} />
       <Route path="login" element={<Authentication />} />
       <Route path="login/callback" element={<Placeholder />} />
       <Route path="*" element={<Placeholder />} />
