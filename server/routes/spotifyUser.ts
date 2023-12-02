a54ca5c107fc368a1d23c2db5c4534c2b65c3a86
Murray Sinclair <msin981@gmail.com>
2023-12-02 22:27:24 +1300

    feat: client APIs to handle user playlist actions

---
+++ server/routes/spotifyUser.ts
@@ -0,0 +1,61 @@
+import { Router } from 'express'
+import request from 'superagent'
+
+const router = Router()
+
+//    /api/v1/user
+
+// CURRENT USER ENDPOINT SCOPES--- UNLOCKED*
+
+// user profile details
+router.get('/:token', async (req, res) => {
+  try {
+    const { token } = req.params
+    const data = await request.get(`https://api.spotify.com/v1/me`).set({
+      Authorization: `Bearer ${token}`,
+    })
+    res.json(data.body)
+  } catch (error) {
+    res.json(error.message)
+  }
+})
+
+// user playlists
+router.get('/playlists/:token', async (req, res) => {
+  try {
+    const { token } = req.params
+    const data = await request
+      .get(`https://api.spotify.com/v1/me/playlists`)
+      .set({
+        Authorization: `Bearer ${token}`,
+      })
+    res.json(data.body)
+  } catch (error) {
+    res.json(error.message)
+  }
+})
+
+// user add to playlist
+router.post('/playlist/:playlistId', async (req, res) => {
+  try {
+    const { playlistId } = req.params
+    const { trackId, token } = req.body
+    const data = await request
+      .post(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`)
+      .set({
+        Authorization: `Bearer ${token}`,
+      })
+      .send({
+        uris: [`spotify:track:${trackId}`],
+        position: 0,
+      })
+    return res.json({ data })
+  } catch (error) {
+    console.error('Error adding tracks to playlist:', error.response.body)
+  }
+})
+
+// remove items in playlist
+// view user tracks in playlist
+
+export default router
