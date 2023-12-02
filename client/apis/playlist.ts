dc37dff2dacacb6fb228e5fdb947ae0ae7b0982f
Murray Sinclair <msin981@gmail.com>
2023-12-02 22:20:26 +1300

    feat: client Apis userPlaylists

--- client/apis/playlist.ts
+++ client/apis/playlist.ts
@@ -1,4 +1,25 @@
-// import request from 'superagent'
+import request from 'superagent'
+import { getSession } from '../functions/startSession'
 
-// const rootUrl = '/api/v1'
+export async function getUserDetails() {
+  const token = await getSession()
+  return await request.get(`https://api.spotify.com/v1/me`).set({
+    Authorization: `Bearer ${token}`,
+  })
+}
 
+export function getUsersPlaylists() {
+  const token = getSession()
+  return request.get(`/api/v1/user/playlists/${token}`)
+}
+
+interface AddTrackToPlaylist {
+  playlistId: string
+  trackId: string
+}
+export function addTrackToPlaylist(data: AddTrackToPlaylist) {
+  const token = getSession()
+  return request
+    .post(`/api/v1/user/playlist/${data.playlistId}`)
+    .send({ trackId: data.trackId, token })
+}
