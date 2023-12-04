4d97955f85b64982e0bbe7fbdd1e1bb2198918b9
Murray Sinclair <msin981@gmail.com>
2023-12-05 10:49:00 +1300

    feat: SpotifyCreatePlaylistButton creates new playlist in spotify

--- client/components/SpotifyCreatePlaylistButton.tsx
+++ client/components/SpotifyCreatePlaylistButton.tsx
@@ -1,24 +1,65 @@
 import e from 'express'
 import React, { useState } from 'react'
 
+// imports that need to refactored into another location
+import request from 'superagent'
+import { getSession } from '../functions/startSession'
+import { getUserDetails } from '../apis/playlist'
+
 const defaultFormData = {
   playlistName: '',
   description: '',
+  public: true,
   theme: '',
 }
 
+const defaultOwner = {
+  playlistId: '',
+  ownerId: '',
+}
+
 function SpotifyCreatePlaylistButton() {
+  //  move playlistDetails to the parent component and refactor the functionality of this to be included in the pipeline of the creating a user playlist __ add this id to the playlist id field,
+
+  // Do the same thing with the ownerDetails,
+
+  // add conditional to the if they are the owner of this playlist,
+
   const [playlistDetails, setPlaylistDetails] = useState(defaultFormData)
+  const [ownerDetails, setOwnerDetails] = useState(defaultOwner)
   const [active, isActive] = useState(false)
 
-  const handleChange = (e: any) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
     setPlaylistDetails({ ...playlistDetails, [name]: value })
   }
 
-  const handleNewPlaylist = async (e: any) => {
-    e.preventDefault(0)
-    // handle the add to spotify using the api and the user Id to generate the playlist for that particular user, also need to add to the database
+  const handleNewPlaylist = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // get the current user id
+    const user_id = await getUserDetails().then((data) => data.id)
+
+    // add a playlist to the current users spotify account
+    const new_playlist = await request
+      .post(
+        `https://api.spotify.com/v1/users/${user_id}/playlists
+    `,
+      )
+      .set({
+        Authorization: `Bearer ${getSession()}`,
+      })
+      .send({
+        name: playlistDetails.playlistName,
+        description: playlistDetails.description,
+        public: playlistDetails.public,
+      })
+      .then((data) => data.body)
+
+    // send this playlist id to the database on creation of a users new playlist
+    setOwnerDetails({
+      playlistId: new_playlist.id,
+      ownerId: user_id,
+    })
   }
 
   return (
