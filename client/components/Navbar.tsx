6238f303fc03a9eb188dd5c8548aa7e817cbde55
Katie Riches <katiegee2011@gmail.com>
2023-12-05 10:04:44 +1300

    Initial commit

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,18 +1,7 @@
-import { Link } from 'react-router-dom'
-
 const NavBar = () => {
   return (
-    <div className="d-flex w-100">
-      <h4 className="mx-2">VibesVault</h4>
-      <Link className="mx-2" to={'/home'}>
-        Home
-      </Link>
-      <Link className="mx-2" to={'/login'}>
-        login
-      </Link>
-      <Link className="mx-2" to={'/login/callback'}>
-        Faulty route
-      </Link>
+    <div>
+      <h1>VibesVault</h1>
     </div>
   )
 }
