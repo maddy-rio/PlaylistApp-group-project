e1a2271f173172bcc5a7cc1d31eb9803fea6c1f6
Murray Sinclair <msin981@gmail.com>
2023-12-04 13:20:27 +1300

    chore: docstring added to userOwnsPlaylist

--- client/functions/userOwnsPlaylist.ts
+++ client/functions/userOwnsPlaylist.ts
@@ -1,7 +1,15 @@
 import { getSession } from './startSession'
 import request from 'superagent'
 
-export async function userOwnsPlaylist(playlistId: string) {
+/**
+ *
+ * @param playlistId takes playlist Id
+ *
+ * check the current logged in user is the owner of the playlist
+ *
+ * @returns boolean
+ */
+export async function userOwnsPlaylist(playlistId: string): Promise<boolean> {
   const user = await request.get('https://api.spotify.com/v1/me').set({
     Authorization: `Bearer ${getSession()}`,
   })
