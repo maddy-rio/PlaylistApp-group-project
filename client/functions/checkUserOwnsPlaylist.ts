58296db00f944daab7ca506d514ca02abb3948d0
Murray Sinclair <msin981@gmail.com>
2023-12-04 13:17:42 +1300

    feat: check logged in user owns playlist

--- client/functions/checkUserOwnsPlaylist.ts
+++ client/functions/checkUserOwnsPlaylist.ts
@@ -1,6 +1,22 @@
-import { spotifyApi } from './startSession'
+import { getSession } from './startSession'
 import request from 'superagent'
 
-async function checkUserOwnsPlalist() {
-  await request.get('https://api.spotify.com/v1/me')
+export async function userOwnsPlaylist(playlistId: string) {
+  const user = await request.get('https://api.spotify.com/v1/me').set({
+    Authorization: `Bearer ${getSession()}`,
+  })
+
+  const playlist = await request
+    .get(
+      `https://api.spotify.com/v1/playlists/${playlistId}
+  `,
+    )
+    .set({
+      Authorization: `Bearer ${getSession()}`,
+    })
+
+  if (user.body.id === playlist.body.owner.id) {
+    return true
+  }
+  return false
 }
