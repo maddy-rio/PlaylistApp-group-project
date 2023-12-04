452ab722618517c4ea7cefef2792d9e0d72f1b80
Murray Sinclair <msin981@gmail.com>
2023-12-05 11:52:53 +1300

    feat: UpdateSpotifyPlatlistButton

---
+++ client/components/UpdateSpotifyPlaylistButton.tsx
@@ -0,0 +1,19 @@
+function UpdateSpotifyPlaylistButton() {
+  // this component should only render if the user is logged in and authenticated
+
+  const handleClick = async (e: any) => {
+    //  update the playlists that are in the spotify playlist
+    //  fetch all the current songs in the database playlist
+    //  fetch all the current songs in the spotify playlist
+    //  find the tracks that dont exist in the spotify playlist
+    //  load all the songs that don't exist into the spotify playlist
+  }
+
+  return (
+    <div className="spotify-update-button">
+      <button>Update the spotify playlist with recent submits</button>
+    </div>
+  )
+}
+
+export default UpdateSpotifyPlaylistButton
