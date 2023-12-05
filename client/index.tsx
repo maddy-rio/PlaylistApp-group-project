38183ed566924d6aa0d07b557bb8b2873f0c03cb
Murray Sinclair <msin981@gmail.com>
2023-12-05 14:53:12 +1300

    chore: format routes

--- client/index.tsx
+++ client/index.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { routes } from './routes'
+import { routes } from './routes/routes'
 
 const queryClient = new QueryClient()
 const router = createBrowserRouter(routes)
