f955570887051492cd6ce58eb93a3c280c9ca5b0
Murray Sinclair <msin981@gmail.com>
2023-12-05 14:50:32 +1300

    feat: adding updatespotifyplaylistbutton

--- client/components/UpdateSpotifyPlaylistButton.tsx
+++ client/components/UpdateSpotifyPlaylistButton.tsx
@@ -1,17 +1,46 @@
-function UpdateSpotifyPlaylistButton() {
+import {
+  dbFetchTrackIds,
+  findPlaylistTracks,
+  getPlaylistItems,
+} from '../apis/playlist'
+
+interface Props {
+  playlistId: number
+}
+
+function UpdateSpotifyPlaylistButton({ playlistId = 2 }: Props) {
   // this component should only render if the user is logged in and authenticated
 
-  const handleClick = async (e: any) => {
+  const handleClick = async () => {
     //  update the playlists that are in the spotify playlist
     //  fetch all the current songs in the database playlist
     //  fetch all the current songs in the spotify playlist
     //  find the tracks that dont exist in the spotify playlist
     //  load all the songs that don't exist into the spotify playlist
+    // await findPlaylistTracks()
+    // const platlistgetPlaylistItems = playlistId
+    const { dbTracks, spotifyPlaylistId } = await dbFetchTrackIds(
+      playlistId,
+    ).then((data) => console.log(data))
+
+    const spotifyTracks = await getPlaylistItems('4BUdzC67m7TKy0oMhCNVbC').then(
+      (data) => data.data.items,
+    )
+    // const spotifyTracks = await getPlaylistItems(spotifyPlaylistId[0].spotify_playlist_id).then((data) => data.data.items)
+    // const dbTrackIds = dbTracks.map((dbTrack:any) => dbTrack.id);
+    console.log(dbTracks);
+    
+    // const uniqueSpotifyTracks = spotifyTracks.filter(dbTracks =>
+    //   !dbTrackIds.includes(spotifyTrack.track.id)
+    // );
   }
+
 
   return (
     <div className="spotify-update-button">
-      <button>Update the spotify playlist with recent submits</button>
+      <button onClick={handleClick}>
+        Update the spotify playlist with recent submits
+      </button>
     </div>
   )
 }
