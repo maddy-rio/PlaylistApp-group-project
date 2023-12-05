ed6c8e82d339e181fc36642fabfb09d1287f3458
Murray Sinclair <msin981@gmail.com>
2023-12-05 17:23:26 +1300

    initial commit esbuild etc

--- server/server.ts
+++ server/server.ts
@@ -6,11 +6,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import spotifyUser from './routes/spotifyUser.ts'
 import tracksRoutes from './routes/tracks.ts'
-<<<<<<< HEAD
-import addUserRouter from './routes/addFunctions.ts'
-=======
 import userPlaylist from './routes/userPlayList.ts'
->>>>>>> main
 
 const server = express()
 
@@ -19,10 +15,8 @@ server.use(cors())
 
 server.use('/api/v1/tracks', tracksRoutes)
 server.use('/api/v1/user/playlists', userPlaylist)
-
 server.use('/api/v1/user', spotifyUser)
-
-server.use('/api/v1/newuser', addUserRouter)
+// server.use('/api/v1/newuser', addUserRouter)
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
