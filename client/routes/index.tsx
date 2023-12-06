e1267268fad67a48f9ab02a1c64e33a056929a8b
Murray Sinclair <msin981@gmail.com>
2023-12-06 14:57:01 +1300

    feat: CurrentPlaylist done

--- client/routes/index.tsx
+++ client/routes/index.tsx
@@ -7,6 +7,7 @@ import NewUser from '../pages/NewUser'
 import Login from '../components/Login'
 import PlaylistPageTemp from '../pages/tobedeleted/PlaylistPageTemp'
 import Layout from '../components/Layout'
+import Songlist from '../components/Songlist'
 
 
 export const routes = createRoutesFromElements(
@@ -14,7 +15,6 @@ export const routes = createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<LandingPage />} />
       <Route path="/newuser" element={<NewUser />} />
-
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/dashboard/:playlistId" element={<CurrentPlaylist />} />
       <Route path="/playlist" element={<PlaylistPageTemp />} />
