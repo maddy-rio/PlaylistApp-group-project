ed51775c5f62f67ed1e41736f0cb09092d861262
Katie Riches <katiegee2011@gmail.com>
2023-12-06 18:04:34 +1300

    Add router functions for Add token to playlsit and get playlist from table- need to do api and component functions.

--- client/pages/Dashboard.tsx
+++ client/pages/Dashboard.tsx
@@ -1,4 +1,13 @@
-import { Flex, Text, Button, Heading, Card, AspectRatio, Dialog, TextField } from '@radix-ui/themes'
+import {
+  Flex,
+  Text,
+  Button,
+  Heading,
+  Card,
+  AspectRatio,
+  Dialog,
+  TextField,
+} from '@radix-ui/themes'
 import { PlusCircledIcon } from '@radix-ui/react-icons'
 import Navigation from '../components/Navigation'
 import gradient from '@privjs/gradients'
@@ -6,12 +15,11 @@ import gradient from '@privjs/gradients'
 import { getUsersPlaylists } from '../apis/playlist'
 import { useMutation, useQuery } from '@tanstack/react-query'
 
-import { Link, useOutletContext } from 'react-router-dom'
+import { Link, useOutletContext, useNavigate } from 'react-router-dom'
 import { ContextType } from '../../models/contextType'
 import Canvas from '../components/Canvas'
 import { useState } from 'react'
 import { addPlaylistToUser } from '../../server/db/functions/getInfo'
-
 
 interface playlistProps {
   playlistsId: string
@@ -20,8 +28,10 @@ interface playlistProps {
 }
 
 const Dashboard = () => {
-  const { userDetails } = useOutletContext<ContextType>() || {};
-  const [form, setForm] = useState({token: ''})
+  const { userDetails } = useOutletContext<ContextType>() || {}
+  const navigate = useNavigate()
+  const [form, setForm] = useState({ token: '' })
+
   const {
     data: playlists,
     error,
@@ -32,8 +42,8 @@ const Dashboard = () => {
   })
   const userMutation = useMutation({
     mutationFn: addPlaylistToUser,
-    onSuccess: () => {
-       window.location.href = `/dashboard/{{playlists_id}}`
+    onSuccess: (data) => {
+      navigate(`/dashboard/${data.playlistsId}`)
     },
   })
 
@@ -45,68 +55,101 @@ const Dashboard = () => {
     return <div>Error</div>
   }
 
-
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     // check later
     e.preventDefault()
-    userMutation.mutate({ ...form, user_id: spotifyId })
+    userMutation.mutate({ ...form, token })
   }
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setForm({ ...form, [e.target.name]: e.target.value })
   }
-  
-
-
-
-
 
   return (
     <>
-    <Flex width="100%" height="100%" className='app'>
-      <Flex direction="column" width="100%" mx="3" my="2" className='fill-height'>
-        <Navigation />
-        <Flex height="100%" m="7">
-          <div className="dashboard">
-            <Heading as="h1" className="dashboard-h1 gradient-text">Hi {userDetails?.display_name}! Here’s all the playlists you’re collaborating on:</Heading>
-            <Flex className="playlist-preview" width="100%" px="8" gap="8" justify="center"wrap="wrap" >
-            {playlists?.map((playlist: playlistProps, index: number) => (
-              <Link to={`/playlist/${playlist.playlistsId}`} key={index}>
-              <Card key={playlist.id} className='playlist-card'>
-                  <Flex direction="column" gap="4" p="2">
-                    <AspectRatio ratio="1/1">
-                      {/* TODO: Add conditional - if no album art, show below div... */}
-                      <div className='album-art' style={{background: gradient(playlist.id) }}></div>
-                      {/* ...otherwise show album art */}
-                    </AspectRatio> 
-                    <Flex direction="column" gap="2">
-                      <svg height="24" width="24" viewBox="0 0 24 24">
-                        <path className="svg-icon-white" d="m11,0C4.92,0,0,4.92,0,11s4.92,11,11,11,11-4.92,11-11S17.08,0,11,0Zm5.04,15.87c-.2.32-.62.43-.94.23-2.58-1.58-5.83-1.94-9.66-1.06-.37.08-.74-.15-.82-.52-.08-.37.15-.74.52-.82,4.19-.96,7.78-.55,10.68,1.23.32.2.43.62.23.94Zm1.35-3c-.25.4-.78.53-1.18.28-2.96-1.82-7.46-2.34-10.96-1.28-.45.14-.93-.12-1.07-.57-.14-.45.12-.93.57-1.07,4-1.21,8.96-.63,12.36,1.46.4.25.53.78.28,1.18Zm.12-3.12c-3.55-2.11-9.39-2.3-12.78-1.27-.54.16-1.12-.14-1.28-.69-.16-.54.14-1.12.69-1.28,3.89-1.18,10.34-.95,14.43,1.47.49.29.65.92.36,1.41-.29.49-.92.65-1.41.36Z"/>
-                      </svg>
-                      <Heading as="h2" className='dashboard-h2'>{playlist.name}</Heading>
-                      <Text className="dashboard-subtitle">Collaborating with: string of users</Text>
+      <Flex width="100%" height="100%" className="app">
+        <Flex
+          direction="column"
+          width="100%"
+          mx="3"
+          my="2"
+          className="fill-height"
+        >
+          <Navigation />
+          <Flex height="100%" m="7">
+            <div className="dashboard">
+              <Heading as="h1" className="dashboard-h1 gradient-text">
+                Hi {userDetails?.display_name}! Here’s all the playlists you’re
+                collaborating on:
+              </Heading>
+              <Flex
+                className="playlist-preview"
+                width="100%"
+                px="8"
+                gap="8"
+                justify="center"
+                wrap="wrap"
+              >
+                {playlists?.map((playlist: playlistProps, index: number) => (
+                  <Link to={`/playlist/${playlist.playlistsId}`} key={index}>
+                    <Card key={playlist.id} className="playlist-card">
+                      <Flex direction="column" gap="4" p="2">
+                        <AspectRatio ratio="1/1">
+                          {/* TODO: Add conditional - if no album art, show below div... */}
+                          <div
+                            className="album-art"
+                            style={{ background: gradient(playlist.id) }}
+                          ></div>
+                          {/* ...otherwise show album art */}
+                        </AspectRatio>
+                        <Flex direction="column" gap="2">
+                          <svg height="24" width="24" viewBox="0 0 24 24">
+                            <path
+                              className="svg-icon-white"
+                              d="m11,0C4.92,0,0,4.92,0,11s4.92,11,11,11,11-4.92,11-11S17.08,0,11,0Zm5.04,15.87c-.2.32-.62.43-.94.23-2.58-1.58-5.83-1.94-9.66-1.06-.37.08-.74-.15-.82-.52-.08-.37.15-.74.52-.82,4.19-.96,7.78-.55,10.68,1.23.32.2.43.62.23.94Zm1.35-3c-.25.4-.78.53-1.18.28-2.96-1.82-7.46-2.34-10.96-1.28-.45.14-.93-.12-1.07-.57-.14-.45.12-.93.57-1.07,4-1.21,8.96-.63,12.36,1.46.4.25.53.78.28,1.18Zm.12-3.12c-3.55-2.11-9.39-2.3-12.78-1.27-.54.16-1.12-.14-1.28-.69-.16-.54.14-1.12.69-1.28,3.89-1.18,10.34-.95,14.43,1.47.49.29.65.92.36,1.41-.29.49-.92.65-1.41.36Z"
+                            />
+                          </svg>
+                          <Heading as="h2" className="dashboard-h2">
+                            {playlist.name}
+                          </Heading>
+                          <Text className="dashboard-subtitle">
+                            Collaborating with: string of users
+                          </Text>
+                        </Flex>
+                        <Button variant="outline" size="2">
+                          <PlusCircledIcon />
+                          Add a song
+                        </Button>
                       </Flex>
-                      <Button variant="outline" size="2">
-                        <PlusCircledIcon />
-                        Add a song
-                      </Button>
-                  </Flex>
-                </Card>
-                </Link>
-                    ))}
-             
-              
-              <Card className='playlist-card'>
-                  <Flex direction="column" gap="4" className='add-playlist-content' p="2">
-                    <AspectRatio ratio={1/1}>
-                      <div className='dashed-box' >
+                    </Card>
+                  </Link>
+                ))}
+
+                <Card className="playlist-card">
+                  <Flex
+                    direction="column"
+                    gap="4"
+                    className="add-playlist-content"
+                    p="2"
+                  >
+                    <AspectRatio ratio={1 / 1}>
+                      <div className="dashed-box">
                         <svg height="36" width="36" viewBox="0 0 56 56">
-                        <path className="plus-icon" d="m26.79,38.63v-9.5h-9.5c-.54,0-.86-.32-.86-.86,0-.43.32-.86.86-.86h9.5v-9.5c0-.43.32-.86.86-.86.43,0,.86.43.86.86v9.5h9.5c.43,0,.86.43.86.86,0,.54-.43.86-.86.86h-9.5v9.5c0,.54-.43.86-.86.86-.54,0-.86-.32-.86-.86Zm28.51-10.37c0,15.34-12.42,27.65-27.65,27.65S0,43.6,0,28.26,12.31.61,27.65.61s27.65,12.42,27.65,27.65ZM27.65,2.34C13.28,2.34,1.73,14.01,1.73,28.26s11.56,25.92,25.92,25.92,25.92-11.56,25.92-25.92S41.91,2.34,27.65,2.34Z"/>
+                          <path
+                            className="plus-icon"
+                            d="m26.79,38.63v-9.5h-9.5c-.54,0-.86-.32-.86-.86,0-.43.32-.86.86-.86h9.5v-9.5c0-.43.32-.86.86-.86.43,0,.86.43.86.86v9.5h9.5c.43,0,.86.43.86.86,0,.54-.43.86-.86.86h-9.5v9.5c0,.54-.43.86-.86.86-.54,0-.86-.32-.86-.86Zm28.51-10.37c0,15.34-12.42,27.65-27.65,27.65S0,43.6,0,28.26,12.31.61,27.65.61s27.65,12.42,27.65,27.65ZM27.65,2.34C13.28,2.34,1.73,14.01,1.73,28.26s11.56,25.92,25.92,25.92,25.92-11.56,25.92-25.92S41.91,2.34,27.65,2.34Z"
+                          />
                         </svg>
                       </div>
                     </AspectRatio>
-                    <Flex className="add-playlist-text" direction="column" gap="2">
-                      <Text className='dashboard-subtitle'>Create a new Spotify playlist or add an existing one</Text>
+                    <Flex
+                      className="add-playlist-text"
+                      direction="column"
+                      gap="2"
+                    >
+                      <Text className="dashboard-subtitle">
+                        Create a new Spotify playlist or add an existing one
+                      </Text>
                     </Flex>
 
                     <Dialog.Root>
@@ -124,35 +167,33 @@ const Dashboard = () => {
                         </Text>
 
                         <Flex direction="column" gap="3">
-                        <form onSubmit={handleSubmit}>
-                          <div>
-                            <label htmlFor="email">Token</label>
-                            <input
-                              type="token"
-                              name="token"
-                              value={form.token}
-                              onChange={handleChange}
-                            />
-                          </div>
-                          <button type="submit">Submit</button>
-                        </form>
+                          <form onSubmit={handleSubmit}>
+                            <div>
+                              <label htmlFor="email">Token</label>
+                              <input
+                                type="token"
+                                name="token"
+                                value={form.token}
+                                onChange={handleChange}
+                              />
+                            </div>
+                            <button type="submit">Submit</button>
+                          </form>
                         </Flex>
                       </Dialog.Content>
                     </Dialog.Root>
-
                   </Flex>
                 </Card>
-            </Flex>
-          </div>
-        </Flex>     
+              </Flex>
+            </div>
+          </Flex>
+        </Flex>
       </Flex>
-    </Flex>
-    <div className='background'>
-    <Canvas />
-    </div>
+      <div className="background">
+        <Canvas />
+      </div>
     </>
   )
-
 }
 
 export default Dashboard
--- server/db/functions/getInfo.ts
+++ server/db/functions/getInfo.ts
@@ -1,5 +1,9 @@
 import connection from '../connection'
-import { PlaylistTrackIds, UserPlaylists, UserName } from '../../../models/getInfo'
+import {
+  PlaylistTrackIds,
+  UserPlaylists,
+  UserName,
+} from '../../../models/getInfo'
 
 const db = connection
 
@@ -26,32 +30,49 @@ export async function getUserPlaylists(
   return playlists
 }
 
-export  async function getUserName( userId: number ): Promise<UserName[]> {
-  const name = await db('users')
-    .where('users.id', userId)
-    .select('name')
-    console.log(`from db function:`, name)
-    return name
+export async function getUserName(userId: number): Promise<UserName[]> {
+  const name = await db('users').where('users.id', userId).select('name')
+  console.log(`from db function:`, name)
+  return name
 }
-
 
 // match a token to a user id and return playlist id
 
-export async function getPlaylistByToken( token: string) {
-  const playlist = await db('playlists')
-    .where('token', token)
-    .select('id')
-    console.log(`from get function:`, playlist)
-    return playlist
+export async function getPlaylistByToken(token: string) {
+  const playlist = await db('playlists').where('token', token).select('id')
+  console.log(`from get function:`, playlist)
+  return playlist
 }
 
+// Adding playlist id and userid to join table
 
-// Adding playlist id and userid to join table 
+export async function addPlaylistToUser(
+  playlistId: number,
+  userId: number,
+  token: string,
+) {
+  try {
+    // Check if the token exists in the playlists table
+    const tokenExists = await db('playlists')
+      .where('playlist_token', token)
+      .first()
 
-export async function addPlaylistToUser(playlistId: number, userId: number) {
-  const playlist = await db('playlists_users')
-    .insert({playlists_id: playlistId, users_id: userId})
-    console.log(`from add function:`, playlist)
-    return playlist
+    // If the token doesn't exist, return an error or handle accordingly
+    if (!tokenExists) {
+      throw new Error('Invalid token')
+    }
+
+    // Token is valid, proceed to insert into the playlists_users table
+    const playlistToUser = await db('playlists_users').insert({
+      playlists_id: playlistId,
+      users_id: userId,
+    })
+
+    console.log(`Successfully added to playlists_users:`, playlistToUser)
+    return playlistToUser
+  } catch (error) {
+    // Handle errors, log, or throw as needed
+    console.error(`Error adding playlist to user:`, error)
+    throw error
+  }
 }
-
